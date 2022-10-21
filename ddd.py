666c653cf1ae6bd9999306d85190ae244132d6b1
Duha-Bakhit <116255991+Duha-Bakhit@users.noreply.github.com>
2022-10-21 13:37:45 +0300

    Update ddd.py

--- ddd.py
+++ ddd.py
@@ -40,7 +40,7 @@ st.bar_chart(dt)
 agree= st.button("click to see Histogram")
 if agree:
     st.markdown("General Histogram for standardized data of various Attributes")
-    histogram(dt)
+    st.histogram(dt)
     
     
 agree= st.button("click to see Area chart")
