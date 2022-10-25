1e42dcab6719c88c27faa29bb191db35e067c7a3
Duha-Bakhit <116255991+Duha-Bakhit@users.noreply.github.com>
2022-10-25 21:52:10 +0300

    Update d.py

--- d.py
+++ d.py
@@ -9,6 +9,9 @@ st.subheader("DAY WISE DATASET AVAILABLE ON KAGGLE")
 st.write("Coronavirus disease (COVID-19) is an infectious disease caused by the SARS-CoV-2 virus. Most people who fall sick with COVID-19 will experience mild to moderate symptoms and recover without special treatment. However, some will become seriously ill and require medical attention.") 
 st.write("The virus can spread from an infected person’s mouth or nose in small liquid particles when they cough, sneeze, speak, sing or breathe. These particles range from larger respiratory droplets to smaller aerosols. You can be infected by breathing in the virus if you are near someone who has COVID-19, or by touching a contaminated surface and then your eyes, nose or mouth. The virus spreads more easily indoors and in crowded settings.")
 
+st.sidebar.title(Visualization Settings)
+st.sidebar.radio(Choose the Chart Type)
+
 @st.cache(suppress_st_warning=True)
 def get_fvalue(val):
     feature_dict = {"No":1,"Yes":2}
